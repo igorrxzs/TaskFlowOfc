50c5837358250e52a0332c4953ae3cd9e3fc79e1
GHbellotto <guibellot121230@gmail.com>
2025-08-28 10:12:44 -0300

    Add files via upload

--- Lista-de-Tarefas-main/paginaDeLogin.js
+++ Lista-de-Tarefas-main/paginaDeLogin.js
@@ -4,7 +4,17 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 });
 
 
-document.getElementById("esqueciSenha").addEventListener("click", function(event) {
+document.getElementById("esqueciSenhaLink").addEventListener("click", function(event) {
   event.preventDefault();  
   window.location.href = "Recuperar Senha.html";  
+});
+//redireciona para cadastrar
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+});
+
+
+document.getElementById("cadastrarLink").addEventListener("click", function(event) {
+  event.preventDefault();  
+  window.location.href = "cadastro.html";  
 });
